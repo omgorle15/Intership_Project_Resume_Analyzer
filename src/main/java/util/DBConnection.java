062866e490ba775e16f7bdc7ba41352c662c7603
omgorle5@gmail.com <omgorle5@gmail.com>
2026-05-07 18:42:14 +0530

    Updated JSP files withot war file

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -10,6 +10,9 @@ public class DBConnection {
     private static final String PASSWORD = "OGsYVwGoeyaSEbYJxgdKcbdBsJGbqiJh";
 //    
 	
+//	 private static final String URL = "jdbc:mysql://localhost:3306/jobanalyzer";
+//	    private static final String USER = "root";
+//	    private static final String PASSWORD = "Omgorle@123";
 	    
     static {
         try {
